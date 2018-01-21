94e6e2025b7f05a4a8ad3325a70cd0c55705d108
Gregory <whatifwedigdeeper@gmail.com>
2018-01-21 12:56:33 -0600

    fs WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('yeah');
+const fs = require('fs');
+const path = require('path');
+const dirname = path.join(__dirname, '../test');
+const docDir = path.join(__dirname, '../docs');
+const files = fs.readdirSync(dirname);
+
+
+const dirExists = (path) => {
+  try  {
+    return fs.statSync(path).isDirectory;
+  }
+  catch (e) {
+    return false;
+  }
+};
+
+if (!dirExists(docDir)) {
+  console.log(`creating directory ${docDir}`);
+  fs.mkdirSync(docDir);
+}
+
+files.forEach(file => {
+  const filePath = path.join(dirname, file);
+  console.log(`filePath ${filePath}`);
+});
